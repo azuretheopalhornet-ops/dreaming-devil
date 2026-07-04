89720235f5baaaec907222479ef1ebbbf71359e1
azuretheopalhornet-ops <azuretheopalhornet@gmail.com>
2026-07-04 12:12:03 +0200

    x

---
+++ localization/en-us.lua
@@ -0,0 +1,12 @@
+return {
+    description = {
+        Joker = {
+            j_kyubey_joker1 = {
+                name = 'joker1',
+                text = {
+                    '{C:chips}+#1#{} chips'
+                }
+            }
+        }
+    }
+}
