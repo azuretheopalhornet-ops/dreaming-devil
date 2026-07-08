8b7764a46df87e3cf5cc78fd10c9cc034582b23e
azuretheopalhornet-ops <azuretheopalhornet@gmail.com>
2026-07-08 13:35:52 +0200

    non ai first joker

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,69 +1,16 @@
 return {
     descriptions = {
         Joker = {
-
-            j_kyubey_silvia = {
+            j_kyubey_Silvia = {
                 name = 'Silvia',
                 text = {
-                    '{C:chips}+#1#{} Chips',
-                    '{C:mult}+#2#{} Mult',
-                    'hey there smexy',
-                    'i have more where.',
-                    'that came from.'
+                    '{C:chips}+#1#{} chips',
+                    '{C:mult}+#2#{} mult',
+                    'Hey smexy I owe',
+                    'the la mafia',
+                    'some money'
                 }
             },
-
-            j_kyubey_paracetamol = {
-                name = 'Paracetamol',
-                text = {
-                    '{C:mult}+#1#{} Mult',
-                    'Loses {C:mult}#2#{} Mult',
-                    'after each scored hand.',
-                    'Resets to {C:mult}+15{}',
-                    'at end of round.'
-                }
-            },
-
-           j_kyubey_mailbox = {
-                name = 'Mailbox',
-                text = {
-                    '{C:mult}+#1#{} Mult',
-                    'Loses {C:mult}-1{} Mult after each blind',
-                    'Also your mail will arrive in 7 blinds'
-                }
-            },
-            j_kyubey_kin = {
-                name = "Kin",
-                text = {
-                    "{C:mult}+12{} Mult.",
-                    "Gives {C:attention}Gold Seals{} to all",
-                    "cards scored in played hand."
-                }
-            },
-            j_kyubey_gin = {
-                name = "Gin",
-                text = {
-                    "{C:chips}+500{} Chips.",
-                    "Turns all cards scored in",
-                    "played hand into {C:attention}Steel Cards{}."
-                }
-            },
-            j_kyubey_twisted_sisters = {
-                name = "Twisted Sisters",
-                text = {
-                    "{X:mult,C:white}X3{} Mult.",
-                    "Turns scored cards into {C:attention}Steel Cards{}",
-                    "with a {C:attention}Gold Seal{}.",
-                    "Scored {C:attention}Steel Cards{} with a {C:attention}Gold Seal{}",
-                    "give {C:mult}+3{} Mult when scored."
-                }
-            }
-        }
-    },
-    misc = {
-        dictionary = {
-            k_fuse = "FUSE",
-            k_cannot_fuse = "NEED BOTH"
         }
     }
-        }
+}
