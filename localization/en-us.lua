19d78955fa27d7e005110a1d86c60db9dce1153c
azuretheopalhornet-ops <azuretheopalhornet@gmail.com>
2026-07-12 17:03:07 +0200

    a

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -18,6 +18,14 @@ return {
                     'halves the current mult',
                     'and reloads at zero mult'
                 }
+            },
+            j_kyubey_shit_a_one = {
+                name = 'Shit a 1',
+                text = {
+                    "Doubles all {C:attention}listed",
+                    "{C:pink,E:1,S:1.1}probabilities",
+                    "{C:inactive}(ex: {C:pink}1 in 3{C:inactive} -> {C:pink}1 in 6{C:inactive})",
+                }
             }
         }
     }
